cdb824ae7aae37764ac9f4ccaf61601cafe4f671
caio17harved <caio.croccia@gmail.com>
2022-04-05 22:07:04 -0300

    2 files modified

--- src/app/juros/juros.component.ts
+++ src/app/juros/juros.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./juros.component.css']
 })
 export class JurosComponent implements OnInit {
+  vp: number;
+  j: number;
+  n: number;
 
-  constructor() { }
+
+  constructor() { 
+    this.vp = 100;
+    this.j = 2.5;
+    this.n = 3;
+  }
 
   ngOnInit() {
+  }
+
+  getJuros(){
+    return this.vp * (1 + this.j/100 * this.n)
   }
 
 }
